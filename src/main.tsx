b3a15eb21b526d1a39c3d1b156dca8628f5a56b4
precki <mpa@AGST-21000374.local>
2024-03-20 23:06:40 +0100

    adding content

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  experience: Experience[];
+  education: string[]
+}
+
+interface Experience {
+  year: number;
+  description: string 
+}
+
+const cvData: CVData = {
+  photo: 'https://dummyimage.com/400x400/d6d6d6/000000.jpg&text=Photo+cover',
+  name: 'Mariusz',
+  lastName: 'Kowalski',
+  position: 'UI Designer',
+  experience: [
+    { year: 2020, description: 'Lorem ipsum' },
+    { year: 2022, description: 'Ala ma kota' },
+    { year: 2024, description: 'Jakaś fajna robota' },
+  ],
+  education: [
+    'Lorem ipsum',
+    'Uniwersytet trzeciego wieku',
+    'Szkoła zycia'
+  ]
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <main>
+      <aside>
+        <header>
+          <h1>CV {cvData.name} {cvData.lastName}</h1>
+        </header>
+        <img src={cvData.photo} alt={`${cvData.name} ${cvData.lastName} profile photo`} />
+        <p>{ cvData.name } { cvData.lastName }</p>
+        <small>{ cvData.position }</small>
+      </aside>
+      <section>
+        <h2>Experience</h2>
+        <ul>
+          {cvData.experience.map(experience => (
+            <li key={experience.year}>
+              <strong>{experience.year}</strong> - {experience.description}
+            </li>
+          ))}
+        </ul>
+        <h2>Education</h2>
+        <ul>
+          {cvData.education.map((education, index) => <li key={index}>{education}</li>)}
+        </ul>
+      </section>
+    </main>
   </React.StrictMode>,
 )
